38a194165182f869acf70fc95fb2448b057419eb
Elizabeth <e.schwartz@wustl.edu>
2023-11-12 14:16:46 -0600

    adding remote repo
    adding remote repo

---
+++ _site/assets/js/lunr-store.js
@@ -0,0 +1,17 @@
+var store = [{  
+"title": "Wright, Richard","date": "1942","creator": "Federal Bureau of Investigation","description": "FBI documents studying Richard Wright.", 
+"id": "object_001.html"
+
+},{  
+"title": "White, Walter","date": "1943","creator": "Federal Bureau of Investigation","description": "FBI documents studying Walter White.", 
+"id": "object_002.html"
+
+},{  
+"title": "Ward, Theodore","date": "1944","creator": "Federal Bureau of Investigation","description": "FBI documents studying Theodore Ward.", 
+"id": "object_003.html"
+
+},{  
+"title": "Smith, William Gardner","date": "1951","creator": "Federal Bureau of Investigation","description": "FBI documents studying William Gardner Smith.", 
+"id": "object_004.html"
+
+}];
